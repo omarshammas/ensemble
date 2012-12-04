7a9af9d94b67cc07fa660388b8938a5ef001f33d
Tunde Agboola <tun.agboola@gmail.com>
2012-12-04 12:10:47 -0500

    Instructions Part 1

--- app/controllers/turk_controller.rb
+++ app/controllers/turk_controller.rb
@@ -5,6 +5,9 @@ class TurkController < ApplicationController
   MIN_THRESHOLD = -2
 
   def tasks
+    if !current_turker.instructed
+      redirect "/instructions?task_id=#{params[:id]}"
+    end
     @task = Task.find(params[:id])
     @comments = @task.comments('created_at asc')
     @suggestions = @task.suggestions.where('sent = :sent AND vote_count > :min_count',{:sent => false, :min_count => MIN_THRESHOLD}).order('vote_count desc')
@@ -16,4 +19,57 @@ class TurkController < ApplicationController
       return render layout: 'turkers_second', action: 'second'
     end
   end
+  
+  def instructions
+    step = session[:step].nil? ? 1: session[:step]
+    @image_url = "#{step}.png"
+    if step == 1
+      @title = "Overview"
+      @description = "An anonymous requestor will provide you with a picture 
+      and a description of an article of clothing he or she desires. Your job is 
+      to to work with others to provide clothing recommendations for the requestor
+      using the interface picture below"
+    elsif step == 2
+      @title = "Requestor Task"
+      @description = "This section displays a picture of the requested article
+      of clothing along with a description. You can click the thumbnail to view
+      a larger image"
+    elsif step == 3
+      @title = "Chat"
+      @description = "As previously mentioned, you will be working with others
+      to create suggestions for the requestor. Use the chat to discuss ideas and
+      viewpoints with other fashionistas!"
+    elsif step == 4
+      @title = "Make a Suggestion"
+      @description = "Click on the (+) icon next to the 'Suggestions' to make a
+      make a suggestion. When making a suggestion paste the URL of the product link
+      and use the arrows to select the preview image of choice along with filling
+      in the rest of the forms"
+    elsif step == 5
+      @title = "Vote on Suggestions"
+      @description = "This section lists all unsent suggestions. You can place an 
+      up vote on a suggestion that you like, or a down vote on a suggestion you 
+      dislike. When a suggestion receives too many down votes it is removed from the
+      list. If it receives enough up votes it will be sent to the requestor"
+    elsif step == 6
+      @title = "Send a Suggestion"
+      @description = "The alert below will pop up when a suggestion has been sent to
+      the requestor. The requestor will either accept the suggestion or reject it 
+      in will be displayed in the feedback section"
+     elsif step == 7
+      @title = "Requestor Feedback"
+      @description = "If a suggestion is rejected it is displayed here. Each rejected 
+      suggestion contains a rating and a comment from the requester"
+     end
+     if(step == 7)
+      task = Task.find(params[:task_id])
+      task.update_attributes(:instructed => true)
+      #TODO link them to task
+     else
+       @next_step = "/turk/instructions?task_id=#{params[:task_id]}"
+     end
+     #session[:step] = step + 1
+     
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121203201432) do
+ActiveRecord::Schema.define(:version => 20121204063634) do
 
   create_table "comments", :force => true do |t|
     t.string   "body"
@@ -83,8 +83,9 @@ ActiveRecord::Schema.define(:version => 20121203201432) do
 
   create_table "turks", :force => true do |t|
     t.string   "code"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "instructed", :default => false
   end
 
   create_table "users", :force => true do |t|
