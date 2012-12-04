6251461a5bcf7f22df2180f30475138194077c3d
Omar Shammas <omar.shammas@gmail.com>
2012-12-04 00:17:41 -0500

    Progress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Ensemble::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( turkers_first.css turkers_second.css )
+  config.assets.precompile += %w( turkers_first.css turkers_second.css first.css second.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
