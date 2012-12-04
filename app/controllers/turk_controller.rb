8e3b0ca63f544e2184bd825c81d42ee0c8810b13
Omar Shammas <omar.shammas@gmail.com>
2012-12-03 21:03:39 -0500

    Updating the rendering of layouts

--- app/controllers/turk_controller.rb
+++ app/controllers/turk_controller.rb
@@ -1,16 +1,19 @@
 class TurkController < ApplicationController
   before_filter :authenticate_turk!
 
-  layout "turkers"
 
   MIN_THRESHOLD = -2
 
-  def task
+  def tasks
     @task = Task.find(params[:id])
     @comments = @task.comments('created_at asc')
     @suggestions = @task.suggestions.where('sent = :sent AND vote_count > :min_count',{:sent => false, :min_count => MIN_THRESHOLD}).order('vote_count desc')
     @history = @task.suggestions.where('sent = :sent AND accepted IS NOT NULL',{:sent => true}).order('created_at desc')
     
-    return render @task.interface
+    if @task.interface == 'first'
+      return render layout: 'turkers_first', action: 'first'
+    elsif @task.interface == 'second'
+      return render layout: 'turkers_second', action: 'second'
+    end
   end
 end
